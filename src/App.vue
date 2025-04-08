3282d6f94d3bad5b07f4904f0e77757e0bddcbbc
caiocroccia22 <caiocroccia22@gmail.com>
2025-04-07 21:22:29 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+// O script -> Onde vai ter o código (lógica funcional)
+// A gente desenvolve elementos html com base nos nossos imports
+const nome = 'Caio Croccia Pereira de Carvalho'
+const meuObj = {
+  nome: 'Caio Croccia',
+  filmeFavorito: 'Creed'
+}
+
+function dizOi(){
+  return `${nome} diz oi`;
+}
+
+const endImagem = "https://static.vecteezy.com/ti/fotos-gratis/t2/36324708-ai-gerado-cenario-do-uma-tigre-caminhando-dentro-a-floresta-foto.jpg"
+
+</script>
+<!-- O vue é separdo em 3 partes  -->
+
+<!-- Bind -> ligar os elementos -->
+
+<template>
+  <!-- template -> Mexe com a estrutura -->
+  <h1>{{ dizOi() }}</h1>
+  <img v-bind:src="endImagem" alt="Imagem tigre">
+</template>
+
+<style scoped>
+/* Style -> Nossa estrutrura de desing */
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+//Função nativa do Vue
+//Permite que crie e monte 
+createApp(App).mount('#app')
